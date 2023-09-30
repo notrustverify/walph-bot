001ca0a1250bd808fc5d131ff72acef10b1a701a
cgi-bin <s@ono.re>
2023-09-30 11:37:06 +0200

    fix: separate each group in own thread pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [
+{
+    name: 'group0',
+    script: 'dist/src/bot.js',
+    args: '0',
+  },
+  {
+    name: 'group1',
+    script: './dist/src/bot.js',
+    args: '1',
+  },
+{
+    name: 'group2',
+    script: 'dist/src/bot.js',
+    args: '2',
+  },
+  {
+    name: 'group3',
+    script: './dist/src/bot.js',
+    args: '3',
+  },
+  ],
+}
+
